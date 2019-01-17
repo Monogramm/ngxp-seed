bff8759d50bb01eb8bd9a611090af75af4789bbb
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-01-17 04:07:42 +0100

    :sparkles: :globe_with_meridians: Add translation depency and user utils

--- src/app/users/user-info/user-details/user-details.component.ts
+++ src/app/users/user-info/user-details/user-details.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectorRef, ChangeDetectionStrategy, Component, EventEmitter, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
-import { User, UserService } from '../../../data';
+import { TranslateService } from '@ngx-translate/core';
+
+import { Logger } from '../../../shared';
+import { LoginService, User, UserService } from '../../../data';
 
 @Component({
     selector: 'user-details',
@@ -12,6 +15,28 @@ import { User, UserService } from '../../../data';
 export class UserDetailsComponent {
     @Input() user: User;
 
-    constructor(public store: UserService) {
+    constructor(
+        public store: UserService, 
+        public loginService: LoginService,
+        private _translate: TranslateService) {
+    }
+
+    sendToken() {
+        if (this.user.email) {
+            this.loginService.sendVerificationToken(this.user.email)
+                .then(
+                    () => {
+                        var msg: string = this._translate.instant('register.message.success.token_sent');
+                        alert(msg);
+                    },
+                    (error) => {
+                        if (Logger.isEnabled) {
+                            Logger.dir(error);
+                        }
+                        var msg: string = this._translate.instant('register.message.error.send_token');
+                        alert(msg);
+                    }
+                );
+        }
     }
 }
