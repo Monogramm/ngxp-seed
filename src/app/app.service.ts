ca958bb6c028fd5f86b2754bd5d3daf87f9c85e2
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-02-02 20:23:27 +0100

    :sparkles: Add PWA update subscription and install prompt catch

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
 
 import { Observable, BehaviorSubject } from 'rxjs';
+
+import { TranslateService } from '@ngx-translate/core';
 
 import { environment } from '../environments/environment';
 
@@ -13,7 +16,37 @@ export class AppService {
 
     title: Observable<string> = this.title$.asObservable();
 
-    constructor() { }
+    pwaBeforeInstallPromptEvent: any = null;
+
+    constructor(private swUpdate: SwUpdate,
+        private translate: TranslateService) {
+        window.addEventListener('beforeinstallprompt', event => {
+            this.pwaBeforeInstallPromptEvent = event;
+            console.log('This app could be installed on your device');
+        });
+
+        this.swUpdate.available.subscribe((evt: UpdateAvailableEvent) => {
+            // an update is available, ask user to confirm reload
+            // go to ngsw-config.json to edit available update content
+            this.confirmReload(evt.available);
+        });
+    }
+
+    promptPwaInstall() {
+        this.pwaBeforeInstallPromptEvent.prompt();
+    }
+
+    confirmReload(available: { hash: string; appData?: Object; }) {
+        this.translate.get('app.message.update', { value: available.appData }).subscribe((msg: string) => {
+            if (confirm(msg) == true) {
+                this.reload();
+            }
+        });
+    }
+
+    reload() {
+        window.location.reload();
+    }
 
     setTitle(title: string) {
         this.title$.next(title);
