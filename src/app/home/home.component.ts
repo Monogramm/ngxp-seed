43350b0813f58f7b815d21f3af42578bb0698ad8
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-02-09 07:00:01 +0100

    :sparkles: Hide logout button in home if not logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,10 @@ export class HomeComponent {
     constructor(public cvm: HomeCommonViewModel,
         private _loginService: LoginService) { }
 
+        isLoggedIn(): boolean {
+            return this._loginService.isLoggedIn();
+        }
+
     logoff() {
         if (Logger.isEnabled) {
             Logger.log('logging off and redirecting to login');
